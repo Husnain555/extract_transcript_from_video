627e5f70a61687e58c1575123c0d4287e8f8e4c5
Husnain555 <husnain.babar.dev@gmail.com>
2025-03-12 16:03:34 +0500

    yt transcript

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from langchain_community.document_loaders import YoutubeLoader
 
 url = input("Enter YouTube URL: ")
-loader = YoutubeLoader.from_youtube_url(url,add_video_info=False)
+loader = YoutubeLoader.from_youtube_url(url, add_video_info=False)
+
 doc = loader.load()
+print(f"Raw Output: {doc}")  # Debugging Step
 
 for do in doc:
-    print(do)
+    print(f"Document: {do.page_content}")  # Debugging Step
